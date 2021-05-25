54cba59a7c1c6e8dfd14f65d733e8b38e4c4c77d
ken505 <68226398+ken505@users.noreply.github.com>
2021-05-25 17:32:04 +0900

    🎉

---
+++ components/Pulus.jsx
@@ -0,0 +1,32 @@
+export function Pulus() {
+  return (
+    // <svg
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   className="h-16 w-16"
+    //   fill="none"
+    //   viewBox="0 0 24 24"
+    //   stroke="currentColor"
+    // >
+    //   <path
+    //     strokeLinecap="round"
+    //     strokeLinejoin="round"
+    //     strokeWidth={2}
+    //     d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+    //   />
+    // </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-20 w-20 text-gray-100 select-none cursor-pointer hover:text-opacity-60 mx-4"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import { DarkModeMenu } from "../components/DarkModeMenu";
+import { Minus } from "../components/Minus";
+import { Pulus } from "../components/Pulus";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-green-400 to-blue-500">
+    <DarkModeMenu />
+      <h1 className="text-4xl text-gray-100 font-bold select-none">Counter</h1>
+      <p className="h-80 w-40 text-9xl text-gray-100 font-bold flex justify-center items-center select-none">
+        9999
+      </p>
+      <div className="flex">
+        <Minus />
+        <Pulus />
+      </div>
+    </div>
+  );
+}
+
+// text-center align-middle
+// <p className="h-55 w-40 text-gray-700 flex justify-center items-center"></p>
