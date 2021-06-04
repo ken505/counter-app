3173c24db3e7e9bb95b63085d7cbb234caf1d9be
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-05 00:16:02 +0900

    ♻️ Change Context file to Arrow function.

--- src/context/AuthUserContext.jsx
+++ src/context/AuthUserContext.jsx
@@ -9,11 +9,13 @@ const authUserContext = createContext({
   signOut: async () => {},
 });
 
-export function AuthUserProvider({ children }) {
+export const AuthUserProvider = (props) => {
   const auth = useFirebaseAuth();
   return (
-    <authUserContext.Provider value={auth}>{children}</authUserContext.Provider>
+    <authUserContext.Provider value={auth}>
+      {props.children}
+    </authUserContext.Provider>
   );
-}
+};
 // custom hook to use the authUserContext and access authUser and loading
 export const useAuth = () => useContext(authUserContext);
