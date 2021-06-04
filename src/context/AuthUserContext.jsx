9f99a43d22721f5ba1bee3968f1398c4333eee80
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-05 00:23:54 +0900

    🐛 Fix a bug.

--- src/context/AuthUserContext.jsx
+++ src/context/AuthUserContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import useFirebaseAuth from "../hooks/useFirebaseAuth";
+import { useFirebaseAuth } from "../hooks/useFirebaseAuth";
 
 const authUserContext = createContext({
   authUser: null,
