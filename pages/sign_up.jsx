daad9ac4266bbd6c389021f7e5643400cf9c6d22
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-04 15:14:45 +0900

    🐛 Fix a bug.

--- pages/sign_up.jsx
+++ pages/sign_up.jsx
@@ -1,18 +1,22 @@
-import { InfoModal } from "../components/InfoModal";
-import { SnsShare } from "../components/SnsShare";
-import { TaskItem } from "../components/TaskItem";
-import { LocalHead } from "../components/LacalHead";
+// import { InfoModal } from "../components/InfoModal";
+// import { SnsShare } from "../components/SnsShare";
+// import { TaskItem } from "../components/TaskItem";
+// import { LocalHead } from "../components/LacalHead";
+// import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-tr from-green-400 dark:from-gray-900 to-blue-400 dark:to-purple-800">
-      <LocalHead />
+      {/* <LocalHead />
       <InfoModal />
       <div>Login</div>
+      <Link href="/">
+      <a>back to index</a>
+      </Link>
       <SnsShare
         url={"https://counter-app-theta.vercel.app/"}
         title={"Counter - App"}
-      />
+      /> */}
     </div>
   );
 };
