d304227dcab30b6a6fd93b950a8fac50fbfd7a43
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-06 00:31:29 +0900

    🐛 Fix a bug.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -43,59 +43,48 @@ const Home = () => {
     >
       <LocalHead />
       <InfoModal />
-      <Container className="text-center" style={{ padding: "40px 0px" }}>
+      <div className="text-center" style={{ padding: "40px 0px" }}>
         <h2 className="text-gray-100 font-mono text-3xl mb-8">Todo-app🐱</h2>
         <p className="text-gray-100 font-mono text-xl mb-1">E-mail</p>
-        <Row style={{ maxWidth: "400px", margin: "auto" }}>
-          <Col>
-            <form onSubmit={onSubmit}>
-              {error && <Alert color="danger">{error}</Alert>}
-              <input
-                className="p-3 rounded-md"
-                type="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                name="email"
-                id="loginEmail"
-                placeholder="Email"
-                size="20"
-              />
-              <FormGroup row>
-                <p className="text-gray-100 font-mono text-xl mt-5 mb-1">
-                  Password
-                </p>
-                {/* <Label for="loginPassword" sm={4}>
-                  Password
-                </Label> */}
-                <Col sm={8}>
-                  <input
-                    className="p-3 rounded-md"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    id="loginPassword"
-                    placeholder="Password"
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup row>
-                <Button className="text-gray-100 font-mono text-3xl font-bold mt-7 mb-5">
-                  Login
-                </Button>
-              </FormGroup>
-              <FormGroup className="text-gray-100 font-mono text-xl mt-5 mb-5">
-                No account?
-                <Link href="/sign_up">
-                  <p className="text-gray-100 font-mono text-xl font-extrabold mt-1 mx-8">
-                    Create one
-                  </p>
-                </Link>
-              </FormGroup>
-            </form>
-          </Col>
-        </Row>
-      </Container>
+        <form onSubmit={onSubmit}>
+          {error && <Alert color="danger">{error}</Alert>}
+          <input
+            className="p-3 rounded-md"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            name="email"
+            id="loginEmail"
+            placeholder="Email"
+            size="20"
+          />
+          <p className="text-gray-100 font-mono text-xl mt-5 mb-1">Password</p>
+          <div>
+            <input
+              className="p-3 rounded-md"
+              type="password"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              id="loginPassword"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <button className="text-gray-100 font-mono text-3xl font-bold mt-7 mb-5">
+              Login
+            </button>
+          </div>
+          <div className="text-gray-100 font-mono text-xl mt-5 mb-5">
+            No account?
+            <Link href="/sign_up">
+              <p className="text-gray-100 font-mono text-xl font-extrabold mt-1 mx-8">
+                Create one
+              </p>
+            </Link>
+          </div>
+        </form>
+      </div>
       <p className="text-gray-100 font-mono text-md mb-3">Trial Account</p>
       <div className="text-gray-100 font-mono text-md">
         <span>Email</span>
@@ -105,10 +94,7 @@ const Home = () => {
         <span>Password</span>
         <span className="font-bold mx-3">123456</span>
       </div>
-      <SnsShare
-        url={"https://counter-app-theta.vercel.app/"}
-        title={"Counter - App"}
-      />
+      <SnsShare />
     </div>
   );
 };
