10361ac4f442de292fbaa972798c322f6a8578db
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-09 16:36:27 +0900

    💬 Changed link name.

--- src/components/LacalHead.jsx
+++ src/components/LacalHead.jsx
@@ -3,14 +3,14 @@ import Head from "next/head";
 export const LocalHead = () => {
   return (
     <Head>
-      <title>Memo App</title>
+      <title>Todo App</title>
       <link rel="icon" href="/favicon.ico" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="og:url" content="https://counter-app-theta.vercel.app/" />
-      <meta property="og:title" content="Memo - App" />
+      <meta property="og:title" content="Todo-App" />
       <meta
         property="og:description"
-        content="This Memo is beautiful and easy to use. ✨"
+        content="This Todo-App is beautiful and easy to use. ✨"
       />
       <meta property="og:image" content="" />
     </Head>
