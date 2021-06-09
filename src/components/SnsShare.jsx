27bb42559ed2b400eacc97cba5dec7081b326f9a
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-09 19:29:15 +0900

    💄 Adjusting the focus ring.

--- src/components/SnsShare.jsx
+++ src/components/SnsShare.jsx
@@ -13,21 +13,29 @@ export const SnsShare = (props) => {
     <div className="flex flex-col fixed bottom-3 right-3 ">
       {/*  */}
       {/* sm:flex-row sm:static  */}
-      <TwitterShareButton url={url} title={title}>
+      <TwitterShareButton
+        className="focus:outline-none focus:ring rounded-md"
+        url={url}
+        title={title}
+      >
         <TwitterIcon
           size={32}
           round
           className="hover:opacity-70 dark:opacity-40 mx-1 my-1"
         />
       </TwitterShareButton>
-      {/* <LineShareButton url={url} title={title}>
+      {/* <LineShareButton
+        className="focus:outline-none focus:ring rounded-md"
+        url={url} title={title}>
         <LineIcon
           size={32}
           round
           className=" hover:opacity-70 dark:opacity-40 mx-1 my-1"
         />
       </LineShareButton>
-      <FacebookShareButton url={url}>
+      <FacebookShareButton
+        className="focus:outline-none focus:ring rounded-md"
+        url={url}>
         <FacebookIcon
           size={32}
           round
