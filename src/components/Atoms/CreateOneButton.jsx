8c97cf2be1952c60e2de8ab2a2581dd155f71a7a
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-13 16:23:45 +0900

    ♻️ Create CreateOneButton.

--- src/components/Atoms/CreateOneButton.jsx
+++ src/components/Atoms/CreateOneButton.jsx
@@ -6,11 +6,11 @@ export const CreateOneButton = () => {
       <Link href="/sign_up">
         <button
           className="text-xs px-5 py-2 mb-8 rounded-full 
-        border border-white dark:border-gray-400 focus:outline-none 
-        bg-gradient-to-tr 
-      from-green-400 dark:from-gray-900 
-      to-blue-400 dark:to-purple-800
-        cursor-pointer  hover:opacity-60 dark:hover:opacity-50"
+          border border-white dark:border-gray-400 focus:outline-none 
+          bg-gradient-to-tr 
+        from-green-400 dark:from-gray-900 
+        to-blue-400 dark:to-purple-800
+          cursor-pointer  hover:opacity-60 dark:hover:opacity-50"
         >
           Create one
         </button>
