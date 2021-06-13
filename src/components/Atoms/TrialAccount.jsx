a402f3e8b86de211a22afeddb12e36c233a4e584
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-13 16:10:53 +0900

    ♻️ Creating Trial Accout component.

---
+++ src/components/Atoms/TrialAccount.jsx
@@ -0,0 +1,15 @@
+export const TrialAccount = () => {
+  return (
+    <div>
+      <p className="text-sm mb-1">Trial Account</p>
+      <div className="text-md">
+        <span className="text-sm">Email</span>
+        <span className="font-bold mx-3">test@tmail.com</span>
+      </div>
+      <div className="text-md ">
+        <span className="text-sm">Password</span>
+        <span className="font-bold mx-3">123456</span>
+      </div>
+    </div>
+  );
+};
