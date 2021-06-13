b4dbbfaba1cf12f460753644421cf911ef6d5d00
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-13 15:56:06 +0900

    ♻️ Improved readability.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   const [error, setError] = useState(null);
   const router = useRouter();
   const { signInWithEmailAndPassword } = useAuth();
-  const onSubmit = (event) => {
+  const onSubmit = (e) => {
     setError(null);
     signInWithEmailAndPassword(email, password)
       .then((authUser) => {
@@ -22,53 +22,56 @@ const Home = () => {
       .catch((error) => {
         setError(error.message);
       });
-    event.preventDefault();
+    e.preventDefault();
   };
   return (
     <div
       className="font-mono text-gray-100 dark:text-gray-400
-    bg-gradient-to-tr from-green-400 dark:from-gray-900 
+      bg-gradient-to-tr 
+    from-green-400 dark:from-gray-900 
     to-blue-400 dark:to-purple-800 
     "
     >
       <LocalHead />
       <InfoModal />
-
       <div className="min-h-screen grid grid-cols-1 items-center text-center ">
         <form onSubmit={onSubmit}>
           <h2 className="text-3xl font-bold mb-6">Todo-app</h2>
           <p className="text-xl mb-1">E-mail</p>
           <div>
             <input
-              className="w-64 p-3 rounded-md text-gray-500 dark:text-gray-300 dark:bg-gray-700 focus:outline-none focus:ring focus:border-blue-300"
+              className="text-gray-500 dark:text-gray-300 
+              w-64 p-3 rounded-md dark:bg-gray-700
+              focus:outline-none focus:ring focus:border-blue-300"
               type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
               name="email"
-              id="loginEmail"
+              value={email}
               placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <p className="text-xl mt-5 mb-1">Password</p>
           <div className="mb-10">
             <input
-              className="w-64 p-3 rounded-md text-gray-500 dark:text-gray-300 dark:bg-gray-700 focus:outline-none focus:ring focus:border-blue-300"
+              className="text-gray-500 dark:text-gray-300
+              w-64 p-3 rounded-md dark:bg-gray-700
+              focus:outline-none focus:ring focus:border-blue-300"
               type="password"
               name="password"
               value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              id="loginPassword"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           {error && <p className="text-red-500">{error}</p>}
           <div>
             <button
-              className="w-60 px-5 py-3 mb-10 text-xl
-            rounded-full border border-white focus:outline-none dark:border-gray-400  
-            bg-gradient-to-tr from-green-400 dark:from-gray-900 
-            to-blue-400 dark:to-purple-800 bg-cover
-            hover:opacity-60 dark:hover:opacity-50"
+              className="text-xl w-60 px-5 py-3 mb-10 
+              rounded-full border border-white dark:border-gray-400 focus:outline-none  
+              bg-gradient-to-tr 
+            from-green-400 dark:from-gray-900 
+            to-blue-400 dark:to-purple-800 
+              hover:opacity-60 dark:hover:opacity-50"
             >
               Login
             </button>
@@ -76,11 +79,12 @@ const Home = () => {
           <p className="text-mb mb-1">No account?</p>
           <Link href="/sign_up">
             <button
-              className="text-xs px-5 py-2 mb-8 rounded-full border
-              border-white focus:outline-none 
-              dark:border-gray-400 cursor-pointer bg-gradient-to-tr from-green-400 dark:from-gray-900 
-              to-blue-400 dark:to-purple-800 bg-cover 
-              hover:opacity-60 dark:hover:opacity-50 "
+              className="text-xs px-5 py-2 mb-8 rounded-full 
+              border border-white dark:border-gray-400 focus:outline-none 
+              bg-gradient-to-tr 
+            from-green-400 dark:from-gray-900 
+            to-blue-400 dark:to-purple-800
+              cursor-pointer  hover:opacity-60 dark:hover:opacity-50"
             >
               Create one
             </button>
