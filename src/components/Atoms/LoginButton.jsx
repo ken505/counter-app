9f19e2ca37a47fdc22f4e1583c67fe65101d766e
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-13 16:10:31 +0900

    ♻️ Creating Login Button.

---
+++ src/components/Atoms/LoginButton.jsx
@@ -0,0 +1,14 @@
+export const LoginButton = () => {
+  return (
+    <button
+      className="text-xl w-60 px-5 py-3 mb-10 
+      rounded-full border border-white dark:border-gray-400 focus:outline-none  
+      bg-gradient-to-tr 
+    from-green-400 dark:from-gray-900 
+    to-blue-400 dark:to-purple-800 
+      hover:opacity-60 dark:hover:opacity-50"
+    >
+      Login
+    </button>
+  );
+};
