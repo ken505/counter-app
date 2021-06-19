6bfc51fba71dbb38ba38d27e0f2ac213ed05fdae
ken505 <68226398+ken505@users.noreply.github.com>
2021-06-19 13:32:17 +0900

    ♻️ Refactor firebase.js code

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,21 @@
 // ↓ firebase SDK のコアを import
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 // ↓ auth と firestore を import
-import 'firebase/auth';
-import 'firebase/firestore';
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
 };
-// Firebaseのインスタンスが存在しない場合は、インスタンスを作成します
+// Firebaseのインスタンスが存在しない場合にのみ、インスタンスを作成します
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp(firebaseConfig);
 }
 export const auth = firebase.auth();
 export const db = firebase.firestore();
